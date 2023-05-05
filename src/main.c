4936c9ca064d177a530808a9bce020b52095aa9a
Tomer <71800501+CASIO15@users.noreply.github.com>
2023-05-05 14:07:04 +0300

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,14 +1,5 @@
 #include "driver.h"
 
-/*
-	TODO:
-	1. Add tests.
-	2. Add memory leak logs.
-	3. Complie again to see if there are no new warnings/errors.
-	6. Refactor some of the code and add dispatch tables where possible.
-	7. add the ability to get string like "abc"d".
-*/
-
 /**
 * Entry point for the driver. This is the entry point for the driver to be executed.
 * 
